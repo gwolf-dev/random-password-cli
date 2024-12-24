2dd80f66a94e9763b396c2894e76c1ac1b9ec5c6
gwolf-dev <gabrielw.ttm@gmail.com>
2024-12-24 16:37:22 -0300

    feat: add prompt user

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 const color = require('cli-color');
+const { prompt, NumberPrompt } = require('enquirer');
 
-const getLanguage = require('./language');
+const translationCLI = require('./language');
+const generatePassword = require('./utils/generatePassword');
 
-const language = getLanguage();
+const translation = translationCLI();
 
-console.log(color.blue(`----- ${language.welcome} -----`));
+console.log(color.blue(`----- ${translation.welcome} ----- \n`));
+
+(async function () {
+  try {
+    const charsLength = new NumberPrompt({
+      name: 'charsLength',
+      message: `${translation.charsLength} ${color.yellow(translation.warningCharsLength)}`,
+    });
+    const answerCharsLength = await charsLength.run();
+
+    const questions = [
+      {
+        type: 'number',
+        name: 'passwdLength',
+        message: translation.passwdLength.replace(
+          '{charsLength}',
+          answerCharsLength,
+        ),
+      },
+      {
+        type: 'multiselect',
+        name: 'charTypes',
+        message: translation.charTypes,
+        choices: [
+          { message: translation.numberSelect, value: 'number' },
+          { message: translation.lowerCaseSelect, value: 'charUpperCase' },
+          { message: translation.upperCaseSelect, value: 'charLowerCase' },
+          { message: translation.symbolsSelect, value: 'symbol' },
+        ],
+        validate: (value) => {
+          if (value.length === 0) {
+            console.warn(color.yellow(translation.invalidOptionsSelect));
+            return false;
+          }
+
+          return true;
+        },
+      },
+    ];
+    const answers = await prompt(questions);
+    const passwordGenerated = generatePassword({
+      ...answers,
+      answerCharsLength,
+    });
+
+    console.log(passwordGenerated);
+  } catch (error) {
+    console.error(color.red(error.message));
+    throw new Error(error);
+  }
+
+  process.exit(1);
+})();
--- language/translation.js
+++ language/translation.js
@@ -1,8 +1,32 @@
 module.exports = {
-    "pt-BR": {
-        welcome: "Olá!!! Esta CLI é destinada para gerar senhas randômicas. Gere e personalise as suas senhas da maneira que quiser :)"
-    },
-    "en-US": {
-        welcome: "Hello!!! This CLI is intended to generate random passwords. Generate and customize your passwords the way you want :)"
-    }
-}
+  'pt-BR': {
+    welcome:
+      'Olá!!! Esta CLI é destinada para gerar senhas randômicas. Gere e personalise as suas senhas da maneira que quiser :)',
+    charsLength: 'Quantos caracteres você deseja na sua senha?',
+    warningCharsLength:
+      '(Lembre-se que uma senha forte tem que ter pelo menos 8 caracteres!)',
+    passwdLength:
+      'Quantas senhas com {charsLength} caracteres, você quer gerar?',
+    charTypes: 'Quais tipos de caracteres deve conter na sua senha?',
+    numberSelect: 'Números',
+    lowerCaseSelect: 'Letra minúscula',
+    upperCaseSelect: 'Letra maiúscula',
+    symbolsSelect: 'Símbolos',
+    invalidOptionsSelect: 'Por favor, selecione uma das opções:',
+  },
+  'en-US': {
+    welcome:
+      'Hello!!! This CLI is intended to generate random passwords. Generate and customize your passwords the way you want :)',
+    charsLength: 'How many characters do you want in your password?',
+    warningCharsLength:
+      '(Remember that a strong password must be at least 8 characters long!)',
+    passwdLength:
+      'How many passwords with {charsLength} characters do you want to generate?',
+    charTypes: 'What types of characters should your password contain?',
+    numberSelect: 'Numbers',
+    lowerCaseSelect: 'Lower case letter',
+    upperCaseSelect: 'Upper case letter',
+    symbolsSelect: 'Symbols',
+    invalidOptionsSelect: 'Please select one of the options:',
+  },
+};

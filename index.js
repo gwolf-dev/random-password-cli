3b947a231d73df6ff44a142ac3f37378bf736a1e
gwolf-dev <gabrielw.ttm@gmail.com>
2024-12-24 14:27:32 -0300

    chore: eslint and prettier config

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const color = require('cli-color');
 
 const getLanguage = require('./language');
 
-
-
 const language = getLanguage();
 
 console.log(color.blue(`----- ${language.welcome} -----`));
-
-
-
-
